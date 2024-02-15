55ca35a91d46cc963de2369e5403a1b7805bb550
EngAndy <andreschelimo@gmail.com>
2024-02-15 13:10:32 +0300

    Init

---
+++ app/src/main/java/com/example/investapp/CreateAccActivity.kt
@@ -0,0 +1,37 @@
+package com.example.investapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class CreateAccActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_acc)
+
+        val back = findViewById<FloatingActionButton>(R.id.back_icon)
+        val register =findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.register)
+        val login = findViewById<TextView>(R.id.login)
+
+
+
+
+        back.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+
+            startActivity(backIntent)
+        }
+        register.setOnClickListener {
+            val registerIntent = Intent( )
+            startActivity(registerIntent)
+        }
+        login.setOnClickListener{
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+
+    }
+}
